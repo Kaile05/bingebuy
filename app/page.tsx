6645107bba25b10dd05e171bc2b2dbb8ec04e180
Kaile05 <lorenzumpad@gmail.com>
2026-09-12 21:46:07 +0800

    feat: configure external images for next image

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,18 @@ export default async function Home() {
   return (
     <main>
       {data.data.products.map(product => (
-        <div>
+        <div key={product.id}>
+          <Image
+            src = {product.image ?? "placeholder.png"}
+            alt = {product.name}
+            width = {200}
+            height= {200}
+           />
           <h1>{product.name}</h1>
           <h2>₱{product.price}</h2>
+          <h2>{product.category}</h2>
+          <h2>{product.rating} ⭐</h2>
+          <h2>{product.stock}</h2>
         </div>
       ))}
     </main>
